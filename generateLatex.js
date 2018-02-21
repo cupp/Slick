74f4984698eee33e636b02c5eb4a3471a6ba1dd9
cupp <cupp@cupps-iMac.local>
2018-02-20 18:09:03 -0800

    update of generateLatex.js

--- generateLatex.js
+++ generateLatex.js
@@ -1,31 +1,43 @@
 var antlr4 = require('antlr4/index');
-//const Slick = require('./SlickListener.js');
 const SlickListener = require('./SlickListener.js').SlickListener;
 const SlickLexer = require('./SlickLexer.js').SlickLexer;
 const SlickParser = require('./SlickParser.js').SlickParser;
 
 // METHOD OVERRIDES
 
-SlickListener.enterEveryRule = (ctx) => {
-
+// SlickListener.prototype.enterEveryRule = (node) => {
+//
+// }
+//
+// SlickListener.prototype.exitEveryRule = (node) => {
+//
+// }
+//
+SlickListener.prototype.enterProof = (ctx) =>  {
+  process.stdout.write("\n\nBEGIN\n\t");
 }
 
-SlickListener.exitEveryRule = (ctx) => {
-
+SlickListener.prototype.exitProof = (ctx) => {
+  process.stdout.write("\nEND\n\n");
 }
 
-SlickListener.enterProof = (ctx) =>  {
-  console.log("Begin");
+SlickListener.prototype.enterExpr = (ctx) => {
+  process.stdout.write("EXPRESSION(");
 }
 
-SlickListener.exitProof = (ctx) => {
-  console.log("End");
+SlickListener.prototype.exitExpr = (ctx) => {
+  process.stdout.write(ctx.getText() + "): " + ctx.getTokens());
 }
 
+SlickListener.prototype.enterJOP
+
+SlickListener.prototype.enterHint = (ctx) => {
+  process.stdout.write("\n\t\tHINT\n\t");
+}
 
 // MAIN
 
-let input = "p ⋁ q =    〈 hint 〉 q ⋁ p ╱╱";
+let input = "\tp ⋁ q\n=\t\t〈 symmetry of ⋁ 〉\n\tq ⋁ p ╱╱";
 
 let chars = new antlr4.InputStream(input);
 let lexer = new SlickLexer(chars);
@@ -34,4 +46,5 @@ let parser = new SlickParser(tokens);
 let printer = new SlickListener();
 parser.buildParseTrees = true;
 let tree = parser.proof();
-antlr4.tree.ParseTreeWalker.DEFAULT.walk(parser, tree);
+console.log(input);
+antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer, tree);
